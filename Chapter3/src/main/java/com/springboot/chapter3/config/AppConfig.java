da29df410981559ec939a072901f3649db362172
XiaomAweII <805122541@qq.com>
2025-04-05 23:55:48 +0800

    深入浅出SpringBoot2.x Chapter3-条件装配Bean

--- Chapter3/src/main/java/com/springboot/chapter3/config/AppConfig.java
+++ Chapter3/src/main/java/com/springboot/chapter3/config/AppConfig.java
@@ -1,9 +1,13 @@
 package com.springboot.chapter3.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.springboot.chapter3.condition.DatabaseConditional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
@@ -23,16 +27,21 @@ import java.util.Properties;
         lazyInit = true)
 public class AppConfig {
 
-    @Bean("druidDataSource")
-    public DataSource getDruidDataSource() {
+    @Bean(value = "druidDataSource", destroyMethod = "close")
+    @Conditional(DatabaseConditional.class)
+    public DruidDataSource getDruidDataSource(
+            @Value("${database.driverName}") String driver,
+            @Value("${database.url}") String url,
+            @Value("${database.username}") String username,
+            @Value("${database.password}") String password) {
         Properties props = new Properties();
-        props.setProperty("driver", "com.mysql.cj.jdbc.Driver");
-        props.setProperty("url", "jdbc:mysql://localhost:3306/chapter");
-        props.setProperty("username", "root");
-        props.setProperty("password", "root");
-        DataSource dataSource = null;
+        props.setProperty("driver", driver);
+        props.setProperty("url", url);
+        props.setProperty("username", username);
+        props.setProperty("password", password);
+        DruidDataSource dataSource = null;
         try {
-            dataSource = DruidDataSourceFactory.createDataSource(props);
+            dataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(props);
         } catch (Exception e) {
             e.printStackTrace();
         }
