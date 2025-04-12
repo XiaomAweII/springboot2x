057555849d5c0582c2dd2f897937a3408be07c5b
XiaomAweII <805122541@qq.com>
2025-04-12 16:55:21 +0800

    深入浅出SpringBoot2.x Chapter3-是用@Profile

--- Chapter3/src/main/java/com/springboot/chapter3/config/AppConfig.java
+++ Chapter3/src/main/java/com/springboot/chapter3/config/AppConfig.java
@@ -4,11 +4,8 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidDataSourceFactory;
 import com.springboot.chapter3.condition.DatabaseConditional;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
@@ -27,18 +24,33 @@ import java.util.Properties;
         lazyInit = true)
 public class AppConfig {
 
+    // 启动项目时配置 JAVA_OPTS="-Dspring.profiles.active=dev" 启动Profile机制
+    @Bean(value = "druidDataSource", destroyMethod = "close")
+    @Profile("dev")
+    public DruidDataSource getDevDruidDataSource() {
+        Properties props = new Properties();
+        props.setProperty("driver", "com.mysql.cj.jdbc.Driver");
+        props.setProperty("url", "jdbc:mysql://localhost:3306/dev_spring_boot?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC");
+        props.setProperty("username", "root");
+        props.setProperty("password", "123456");
+        DruidDataSource dataSource = null;
+        try {
+            dataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
     @Bean(value = "druidDataSource", destroyMethod = "close")
     @Conditional(DatabaseConditional.class)
-    public DruidDataSource getDruidDataSource(
-            @Value("${database.driverName}") String driver,
-            @Value("${database.url}") String url,
-            @Value("${database.username}") String username,
-            @Value("${database.password}") String password) {
+    @Profile("test")
+    public DruidDataSource getTestDruidDataSource() {
         Properties props = new Properties();
-        props.setProperty("driver", driver);
-        props.setProperty("url", url);
-        props.setProperty("username", username);
-        props.setProperty("password", password);
+        props.setProperty("driver", "com.mysql.cj.jdbc.Driver");
+        props.setProperty("url", "jdbc:mysql://localhost:3306/test_spring_boot?useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC");
+        props.setProperty("username", "root");
+        props.setProperty("password", "123456");
         DruidDataSource dataSource = null;
         try {
             dataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(props);
